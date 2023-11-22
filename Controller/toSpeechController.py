33c5fb01d5f26bfbf409f13d45f24d3f11fd9c95
thoai.nguyen <thoaint.dev@gmail.com>
2023-11-22 16:58:50 +0700

    source demo

---
+++ Controller/toSpeechController.py
@@ -0,0 +1,31 @@
+import pyttsx3, base64, os
+
+
+
+def fn_create_speech(text):
+    # text = request.data.decode('utf-8')
+    file_name = "file.mp3"
+    engine = pyttsx3.init()
+    voices = engine.getProperty("voices")
+    vi_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\MSTTS_V110_viVN_An"
+    engine.setProperty("voice", vi_voice_id)
+    engine.save_to_file(text, file_name)
+    engine.runAndWait()
+    
+    rename = move_file_dicretory(file_name)
+
+    with open(rename, 'rb') as f:
+        mp3_bytes = f.read()
+
+    mp3_file = base64.b64encode(mp3_bytes).decode("utf-8")
+
+    return mp3_file
+
+def move_file_dicretory(file_name):
+    path = './root/file_speech/'
+    rename = path + file_name
+    
+    if os.path.exists(rename):
+        os.remove(rename)
+    os.rename(file_name, rename)
+    return rename
--- config.py
+++ config.py
@@ -6,4 +6,4 @@ class ConfigDB:
     SECRET_KEY = 'verysecretkey'
     CONNECT_DB = False
     SQLDATABASE_URI = 'mssql+pyodbc://ebm:4OcDQ4OLo5eGngU@103.127.207.180,3968/EBM_CRM_TEST_SE?driver=ODBC+Driver+17+for+SQL+Server'
-    SQLDATABASE_INCLUDE_TABLES = ["Division","Class","Student","StudentAbsent", "StudentDept", "StudentClass", "AccountingVoucher", "Fund", "FundDetails"]
+    SQLDATABASE_INCLUDE_TABLES = ["Division","Class","Student","StudentAbsent", "StudentDept", "StudentClass", "StudentClassFee", "TrackClass"]
