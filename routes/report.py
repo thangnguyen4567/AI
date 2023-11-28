1dc6ca8ba7f93f73c8774667c16dc5006d6b6954
thang.nguyen <thang.nguyen@vnresource.org>
2023-11-28 08:34:28 +0700

    Bổ sung controller report v2

--- routes/report.py
+++ routes/report.py
@@ -1,8 +1,14 @@
 from flask import request,jsonify,Blueprint
-from Controller.answerQuerySQL import get_conversation_query_sql
+from Controller.reportController import get_conversation_query_sql
+from Controller.testController2 import test
 
 report = Blueprint('report', __name__)
 
 @report.route('/api/answer_query', methods=['POST'])
 def create_query_sql():
     return jsonify(get_conversation_query_sql(request))
+
+
+@report.route('/api/answer_query_v2', methods=['POST'])
+def create_query_sql_custom():
+    return jsonify(test(request))
