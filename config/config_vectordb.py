76d9bad7b6b95137c9e8262fa2fb48dae5c5981a
thoai.nguyen <thoaint.dev@gmail.com>
2023-12-15 10:16:07 +0700

    Update config redis-stack

--- config/config_vectordb.py
+++ config/config_vectordb.py
@@ -2,6 +2,8 @@ from langchain.schema import Document
 from langchain.vectorstores import faiss
 from langchain.embeddings import HuggingFaceHubEmbeddings
 import pandas as pd
+from langchain.vectorstores.redis import Redis
+
 def config_vectordb():
     embeddings = HuggingFaceHubEmbeddings()
     data = pd.read_excel("./document/data.xlsx",usecols=['question', 'answer'])
@@ -12,5 +14,20 @@ def config_vectordb():
         Document(page_content=question, metadata={"query": few_shots[question]})
         for question in few_shots.keys()
     ]
-    vector_db = faiss.FAISS.from_documents(newDocs, embeddings)
+
+    vector_db = redis_vector(embeddings, newDocs)
+    # vector_db = faiss.FAISS.from_documents(newDocs, embeddings)
+    
     return vector_db
+
+
+def redis_vector(embeddings, documents):
+    redis_url = "redis://127.0.0.1:6380"
+    index_name = 'rds_index'
+    rds = Redis.from_documents(
+        documents=documents,
+        embedding=embeddings,
+        index_name=index_name,
+        redis_url=redis_url
+    )
+    return rds
