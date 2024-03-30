137fc7e6ed7c9be8fe362cd381055208fbc7d64f
thang.nguyen <thang.nguyen@vnresource.org>
2024-03-30 10:09:20 +0700

    Bổ sung route giọng nói

--- routes/speech.py
+++ routes/speech.py
@@ -1,21 +1,15 @@
-from controller.toSpeechController import fn_create_speech
-from flask import Blueprint,jsonify, render_template,request
+from flask import Blueprint,send_file,request
+import pyttsx3
+
 speech = Blueprint('speech', __name__)
 
-
-
-@speech.route('/api/view_speech', methods=['GET','POST'])
-def speech_view():
-    strBase64 = ''
-    if request.method == 'POST':
-        text = request.form['input']
-        strBase64 = fn_create_speech(text)
-        return render_template("text_speech.html", url_base64 = strBase64)
-    else:
-        return render_template("text_speech.html", url_base64 = strBase64)
-
-
-@speech.route('/api/create_speak', methods=['POST'])
+@speech.route('/api/voice', methods=['POST'])
 def api_create_speech():
-    text = request.data.decode('utf-8')
-    return jsonify({'mp3_file': fn_create_speech(text)})
+    if 'text' in request.get_json() :
+        text = request.get_json()['text']
+        engine = pyttsx3.init()
+        voices = engine.getProperty("voices")
+        engine.setProperty("voice", voices[1].id)
+        engine.save_to_file(text, 'speech.mp3')
+        engine.runAndWait()
+    return send_file('speech.mp3', mimetype='audio/mpeg')
