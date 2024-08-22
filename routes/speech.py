7edb830db09f522418598b6200f5f66391de4711
lethanhvuvnr <vu.lethanh@vnresource.net>
2024-08-22 15:16:02 +0700

    Xử lý route text to speech

--- routes/speech.py
+++ routes/speech.py
@@ -2,6 +2,8 @@ from flask import Blueprint,send_file,request
 import pyttsx3
 from gtts import gTTS
 from pydub import AudioSegment
+import uuid
+import io
 
 speech = Blueprint('speech', __name__)
 
@@ -16,6 +18,23 @@ def api_create_speech():
         faster_audio.export("speech.mp3", format="mp3")
     return send_file('speech.mp3', mimetype='audio/mpeg')
 
+@speech.route('/voice_custom', methods=['POST'])
+def api_create_speech_custom():
+    if 'text' in request.get_json() :
+        text = request.get_json()['text']
+        
+        tts = gTTS(text=text, lang='vi')
+        audio_buffer = io.BytesIO()
+        tts.write_to_fp(audio_buffer)
+        
+        audio_buffer.seek(0)
+        audio = AudioSegment.from_file(audio_buffer, format="mp3")
+        faster_audio = audio.speedup(playback_speed=1.3)
+        
+        output_buffer = io.BytesIO()
+        faster_audio.export(output_buffer, format="mp3")
+        output_buffer.seek(0)
 
-
-
+        return send_file(output_buffer, mimetype='audio/mpeg')
+    else:
+        return 'Không có dữ liệu'
