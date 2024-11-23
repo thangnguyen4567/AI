d42d340d8c6d88cb52a248f324db413c271dd21b
thang.nguyen <thang.nguyen@vnresource.org>
2024-11-23 11:33:26 +0700

    Cập nhật 1 số tool cho langgraph

---
+++ project/lms/tools/search_student_course.py
@@ -0,0 +1,27 @@
+from langchain.agents import tool
+import requests
+from langchain_core.runnables import RunnableConfig
+
+@tool
+def search_student_course(time: str, config: RunnableConfig) -> list:
+    """ Tìm kiếm dánh sách khóa học đã được ghi danh của học viên""" 
+    
+    configuration = config.get("configurable", {})
+    data = {
+        "userid": configuration.get('userid'),
+        "time": int(time.timestamp())
+    }
+    token = configuration.get('token')
+    endpoint = configuration.get('endpoint')
+    headers = {
+        "Accept-Charset": "",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer "+token,
+    }
+
+    url = endpoint+"/api/integrated/ai/student-courses"
+    try:
+        response = requests.get(url, json=data, headers=headers)
+        return str(response.json()['data'])
+    except requests.exceptions.RequestException as e:
+        return 'Không tìm thấy khóa học'
